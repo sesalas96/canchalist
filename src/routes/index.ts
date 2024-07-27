5a6d6e0757515108331a9c7feafce694e5120f88
Sean Siefert <sean.siefert@amiggi.com>
2024-07-26 21:52:11 -0700

    Test bucket list

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from 'express';
-import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+import { S3Client, ListBucketsCommand, ListObjectsCommand } from '@aws-sdk/client-s3';
 
 const router = Router();
 const client = new S3Client({ region: "us-east-1" });
 
 router.get('/', async (req: Request, res: Response) => {
-  const command = new ListBucketsCommand({ });
+  const command = new ListObjectsCommand({ Bucket: 'ccitmpsom'});
   let baseData = JSON.stringify(process.env) + "\n"
   try {
     const data = await client.send(command);
