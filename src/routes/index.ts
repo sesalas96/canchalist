b5c381ab98692b6977d8dab440ef559f69e2eab4
Sean Siefert <sean.siefert@amiggi.com>
2024-07-30 16:14:43 -0700

    Upload test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { S3Client, ListBucketsCommand, ListObjectsCommand } from '@aws-sdk/client-s3';
+import { S3Client, ListBucketsCommand, ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const router = Router();
 const client = new S3Client({ region: "us-east-1" });
@@ -8,10 +8,21 @@ router.get('/', async (req: Request, res: Response) => {
   const bucket = (typeof req.query.bucket === "string") ? req.query.bucket : "";
 
   if (bucket) {
+    let key = Math.random() * 1000000;
+
+
+    const input = {
+      "Body": "HappyFace.jpg",
+      "Bucket": "ccitmpsom",
+      "Key": "test" + key + ".txt"
+    };
+
     const command = new ListObjectsCommand({ Bucket: bucket });
     let baseData = JSON.stringify(process.env) + "\n"
     try {
       const data = await client.send(command);
+      const writeCommand = new PutObjectCommand(input)
+      await client.send(writeCommand);
       res.send(baseData + JSON.stringify(data))
       // process data.
     } catch (error) {
