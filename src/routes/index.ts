0ffb29346f3371d50e37443d7da7e839d6c62d32
Sean Siefert <sean.siefert@amiggi.com>
2024-07-31 21:45:18 -0700

    Test kafka

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,40 +1,69 @@
 import { Router, Request, Response } from 'express';
-import { S3Client, ListBucketsCommand, ListObjectsCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import { Kafka } from 'kafkajs';
+const {
+  createMechanism
+} = require('@jm18457/kafkajs-msk-iam-authentication-mechanism')
+
 
 const router = Router();
-const client = new S3Client({ region: "us-east-1" });
+
+let kafka =new Kafka({
+  clientId: 'test-app',
+  brokers: ['b-1.ccikafkadev.blxpf3.c21.kafka.us-east-1.amazonaws.com:9098','b-2.ccikafkadev.blxpf3.c21.kafka.us-east-1.amazonaws.com:9098','b-3.ccikafkadev.blxpf3.c21.kafka.us-east-1.amazonaws.com:9098'],
+  // authenticationTimeout: 10000,
+  // reauthenticationThreshold: 10000,
+  ssl: true,
+  sasl: createMechanism({ region: 'us-east-1' })/*{
+      mechanism: 'aws',
+      authorizationIdentity: 'AIDAIOSFODNN7EXAMPLE', // UserId or RoleId
+      accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+      secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+      sessionToken: 'WHArYt8i5vfQUrIU5ZbMLCbjcAiv/Eww6eL9tgQMJp6QFNEXAMPLETOKEN' // Optional
+    },*/
+})
+
+let isConsuming = false;
+
+
 
 router.get('/', async (req: Request, res: Response) => {
-  const bucket = (typeof req.query.bucket === "string") ? req.query.bucket : "";
+  // check msk topcs
 
-  if (bucket) {
-    let key = Math.random() * 1000000;
+  if (!isConsuming) {
+    isConsuming = true
+    const consumer = kafka.consumer({ groupId: 'testgroup' })
+    await consumer.subscribe({ topics: ['on-test'] })
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
 
-
-    const input = {
-      "Body": "HappyFace.jpg",
-      "Bucket": "ccitmpsom",
-      "Key": "test" + key + ".txt"
-    };
-
-    const command = new ListObjectsCommand({ Bucket: bucket });
-    let baseData = JSON.stringify(process.env) + "\n"
-    try {
-      const data = await client.send(command);
-      const writeCommand = new PutObjectCommand(input)
-      await client.send(writeCommand);
-      res.send(baseData + JSON.stringify(data))
-      // process data.
-    } catch (error) {
-      res.send(baseData + JSON.stringify(error))
-      // error handling.
-    } finally {
-      // finally.
-    }
+        console.log({
+          // @ts-ignore
+          key: message.key.toString(),
+          // @ts-ignore
+          value: message.value.toString(),
+          headers: message.headers,
+        })
+      },
+    })
+    res.send("started listening")
   }
   else {
-    res.send("pass a bucket param to test connectivity")
+    res.send("already listening")
   }
+
+  /*const params = {
+  };
+  const command = new ListClustersCommand(params);
+  try {
+    const data = await client.send(command);
+    res.send(JSON.stringify(data))
+    // process data.
+  } catch (error) {
+    // error handling.
+    res.send(JSON.stringify(error));
+  } finally {
+    // finally.
+  }*/
 
 });
 
