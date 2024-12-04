578768c367e1020412d3bf8322a861fefd3832b0
Sebastián Salas <sebashian961@gmail.com>
2024-12-04 10:01:20 -0600

    init landin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { ROUTES } from '@src/constants';
 import usersRouter from './userRoutes';
-import groupRoutes from './groupRoutes';
-import matchesRoutes from './matchesRoutes';
+import groupRouter from './groupRoutes';
+import matchesRouter from './matchesRoutes';
 import miscRouter from './misc';
 
 const router = Router();
 
 router.use(ROUTES.USERS.BASE, usersRouter);
-router.use(ROUTES.GROUPS.BASE, groupRoutes);
-router.use(ROUTES.MATCHES.BASE, matchesRoutes);
+router.use(ROUTES.GROUPS.BASE, groupRouter);
+router.use(ROUTES.MATCHES.BASE, matchesRouter);
 router.use(miscRouter);
 
 export default router;
