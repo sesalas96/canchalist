5c17913c48e8a8944368dad5b6dc3c745f9d0bfe
Sebastián Salas <sebashian961@gmail.com>
2024-12-07 18:00:30 -0600

    Init

--- src/routes/13_catalogRoutes.ts
+++ src/routes/13_catalogRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { getAllCatalogs, getCatalogByName } from '@src/controllers/13_catalogController';
 import { ROUTES } from '@src/constants';
+import { auth } from '@src/middlewares/authenticate';
 
 const router = Router();
 
 // Ruta para obtener todos los catálogos
-router.get(ROUTES.CATALOGS.BASE, getAllCatalogs);
+router.get(ROUTES.CATALOGS.BASE, auth, getAllCatalogs);
 
 // Ruta para obtener un catálogo específico por nombre
-router.get(ROUTES.CATALOGS.BY_ID, getCatalogByName);
+router.get(ROUTES.CATALOGS.BY_ID, auth, getCatalogByName);
 
 export default router;
